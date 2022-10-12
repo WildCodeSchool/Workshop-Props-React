81454be4b003871ea64034e48efa18f065eaccb2
David Faure <aswelldavid@gmail.com>
2022-10-12 10:42:05 +0200

    correction done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,9 +4,32 @@ const Profile = () => {
   return (
     <section className="container">
       {/* props are missing here */}
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard
+        firstName="David"
+        lastName="Faure"
+        userEmail="user@example.com"
+        userAdress="10 rue des David, 33000 Bordeaux"
+        buttonLabel="Plus d'infos"
+        color="blue"
+        button
+      />
+      <ProfileCard
+        firstName="Florian"
+        lastName="Angular"
+        userEmail="user@example.com"
+        userAdress="10 rue des Florian, 33000 Bordeaux"
+        buttonLabel="Plus d'infos"
+        color="red"
+      />
+      <ProfileCard
+        firstName="Jean"
+        lastName="Le Grand"
+        userEmail="user@example.com"
+        userAdress="10 rue des Grands, 33000 Bordeaux"
+        buttonLabel="Plus d'infos"
+        color="green"
+        button
+      />
     </section>
   );
 };
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,15 +1,27 @@
-const ProfileCard = () => {
+const ProfileCard = ({
+  firstName,
+  lastName,
+  userEmail,
+  userAdress,
+  buttonLabel,
+  color,
+  button,
+}) => {
   //todo props are missing here
   return (
     <section className="profile__container">
-      <h2 className="profile__blue">Nom Prénom</h2>
+      <h2 className={`profile__${color}`}>
+        {firstName} {lastName}{" "}
+      </h2>
       <p>
-        <strong>Email</strong> : mon email
+        <strong>Email</strong> : {userEmail}
       </p>
       <p>
-        <strong>Adresse</strong> : mon adresse{" "}
+        <strong>Adresse</strong> : {userAdress}
       </p>
-      <button className="button__offer bck__blue">Label</button>
+      {button && (
+        <button className={`button__offer bck__${color}`}>{buttonLabel}</button>
+      )}{" "}
     </section>
   );
 };
